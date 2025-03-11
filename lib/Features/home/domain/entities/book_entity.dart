25c40bf15fea83aa855958504e62804c014c62a2
mohamedredaelsheikh <moh.reda198@gmail.com>
2025-03-11 05:54:44 +0200

    Add Book Entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String title;
+  final String author;
+  final String image;
+  final double price;
+  final double rate;
+
+  BookEntity(
+      {required this.title,
+      required this.author,
+      required this.image,
+      required this.price,
+      required this.rate});
+}
