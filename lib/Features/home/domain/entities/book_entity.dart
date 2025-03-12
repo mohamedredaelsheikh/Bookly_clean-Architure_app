b94f0cee5d1dff37220886c32e720d4820ab7f92
mohamedredaelsheikh <moh.reda198@gmail.com>
2025-03-12 04:44:49 +0200

    craete Book Model.

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,12 +1,14 @@
 class BookEntity {
+  final String bookId;
   final String title;
-  final String author;
-  final String image;
-  final double price;
-  final double rate;
+  final String? author;
+  final String? image;
+  final num? price;
+  final num? rate;
 
   BookEntity(
       {required this.title,
+      required this.bookId,
       required this.author,
       required this.image,
       required this.price,
